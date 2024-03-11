b657d9644e478c334573cb2325a43e7811be13c1
Julian-Expo <126873290+Julian-Expo@users.noreply.github.com>
2024-03-11 16:57:52 +0000

    Initial Upload

---
+++ Init-VM.ps1
@@ -0,0 +1,22 @@
+& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"en-gb.xml`""
+Set-WinSystemLocale -SystemLocale en-GB
+Set-WinHomeLocation -GeoId 242
+Set-WinUserLanguageList -LanguageList (New-WinUserLanguageList -Language en-GB) -Force
+Set-Culture en-GB
+Start-Sleep -Seconds 40
+mkdir C:\windows\system32\config\systemprofile\AppData\Local\Temp\
+Copy-Item ./securitypolicy.inf -Destination C:\windows\system32\config\systemprofile\AppData\Local\Temp\ -Force
+Start-sleep -Seconds 10
+#$currentDriveLetters=Get-Volume | Where-Object{$_.DriveLetter} | Select -ExpandProperty DriveLetter
+#$lastDriveLetterChar=([int[]][char[]]$currentDriveLetters | Measure -Maximum | select -ExpandProperty Maximum)
+#Get-Disk | Where-Object{$_.IsOffline -eq $true} | Set-Disk -IsOffline $false
+#$disks=Get-Disk | Where-Object{$_.PartitionStyle -eq "RAW"}
+#$disks | Initialize-Disk -PartitionStyle GPT
+#foreach($disk in $disks)
+#{
+#    $lastDriveLetterChar = $lastDriveLetterChar + 1
+#    $disk | New-Partition -UseMaximumSize -DriveLetter "$([char][int]$lastDriveLetterChar)"
+#    Format-Volume -DriveLetter "$([char][int]$lastDriveLetterChar)" -FileSystem "NTFS" -Full
+#}
+Restart-Computer
+exit 0
